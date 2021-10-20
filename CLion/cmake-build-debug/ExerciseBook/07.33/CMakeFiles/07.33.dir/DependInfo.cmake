6356a3e8164837930935d50a6e74e3b2ee05cd4c
陆学嘉 <luxuejia@ubox.cn>
2021-10-20 11:38:00 +0800

    添加git忽略文件

---
+++ CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/project/c++/Data-Structure/CLion/ExerciseBook/07.33/07.33.c" "D:/project/c++/Data-Structure/CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/07.33.c.obj"
+  "D:/project/c++/Data-Structure/CLion/ExerciseBook/07.33/CSTree.c" "D:/project/c++/Data-Structure/CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/CSTree.c.obj"
+  "D:/project/c++/Data-Structure/CLion/ExerciseBook/07.33/Graph.c" "D:/project/c++/Data-Structure/CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/Graph.c.obj"
+  "D:/project/c++/Data-Structure/CLion/ExerciseBook/07.33/MFSet.c" "D:/project/c++/Data-Structure/CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/MFSet.c.obj"
+  "D:/project/c++/Data-Structure/CLion/ExerciseBook/07.33/MinimumSpanningTree.c" "D:/project/c++/Data-Structure/CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/MinimumSpanningTree.c.obj"
+  "D:/project/c++/Data-Structure/CLion/ExerciseBook/07.33/SqQueue.c" "D:/project/c++/Data-Structure/CLion/cmake-build-debug/ExerciseBook/07.33/CMakeFiles/07.33.dir/SqQueue.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Status"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/project/c++/Data-Structure/CLion/cmake-build-debug/Status/CMakeFiles/Scanf_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
